490155bfc9f6c2ffc1a26173b89a7b8a59c078fc
EdinSehic <esehic@dev-10.com>
2025-03-27 15:13:20 -0500

    Renamed and removed columns in data sets. Made some edits to sql schema.

--- boardgame-schema-data.sql
+++ boardgame-schema-data.sql
@@ -2,47 +2,46 @@ drop database if exists boardgame;
 create database boardgame;
 use boardgame;
 
-create table boardgame(
-boardgame_id int primary key auto_increment,
-id int,
-boardgame_name varchar(255) not null,
-yearpublished int
+create table boardgame (
+    boardgame_id int primary key auto_increment,
+    bgg_id int unique, 
+    boardgame_name varchar(255) not null,
+    year_published int
 );
 
-create table boardgame_primary(
-boardgame_primary_id int primary key auto_increment,
-boardgame_id int,
+create table boardgame_primary (
+    boardgame_primary_id int primary key auto_increment,
+    boardgame_id int,
+    
+    boardgame_rank int,
+    bayes_average float,
+    average float,
+    users_rated int,
+    is_expansion boolean,
+    abstracts_rank int null,
+    cgs_rank int null,
+    childrensgames_rank int null,
+    familygames_rank int null,
+    partygames_rank int null,
+    strategygames_rank int null, 
+    thematic_rank int null,
+    wargames_rank int null,
 
-boardgame_rank int,
-bayesaverage float,
-average float,
-usersrated int,
-is_expansion boolean,
-abstracts_rank int,
-cgs_rank int,
-childrensgames_rank int,
-familygames_rank int,
-partygames_rank int,
-strategygames_rank int, 
-thematic_rank int,
-wargames_rank int,
-
-foreign key (boardgame_id) references boardgame(boardgame_id)
+    foreign key (boardgame_id) references boardgame(boardgame_id) on delete cascade
 );
 
-create table boardgame_secondary(
-boardgame_secondary_id int primary key auto_increment,
-boardgame_id int,
+create table boardgame_secondary (
+    boardgame_secondary_id int primary key auto_increment,
+    boardgame_id int,
+    
+    min_players int,
+    max_players int,
+    play_time int,
+    min_age int,
+    users_rated int,
+    rating_average float,
+    complexity_average float,
+    owned_users int,
 
-min_players int,
-max_players int,
-play_time int,
-min_age int,
-users_rated int,
-rating_average float,
-bgg_rank int,
-complexity_average float,
-owned_users int,
-
-foreign key (boardgame_id) references boardgame(boardgame_id)
+    foreign key (boardgame_id) references boardgame(boardgame_id) on delete cascade
 );
